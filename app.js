e48e4393c945e279dc52fc9c5facccc2060f5f36
. TEMILOLUWA. <eoni1849@gmail.com>
2025-04-07 06:02:56 +0100

    fixed bug in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import router from "../routes/index.js"
+import router from "./routes/index.js"
 
 router.use('/api/v1', router)
 
